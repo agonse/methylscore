c1d93cf46e53444ea4ff3e6c48ad1c904c607e54
Alex G Segura <83952268+agonse@users.noreply.github.com>
2024-12-19 09:10:00 +0100

    Update calculate_multiple_episcores.R

--- R/calculate_multiple_episcores.R
+++ R/calculate_multiple_episcores.R
@@ -2,38 +2,38 @@
 #'
 #' This function calculates multiple epigenetic scores using methylation information.
 #' @description
-#' This function calculates multiple epigenetic score based on methylation data. Two inputs are required: a beta matrix with CpG methylation and a list of external references with the association and effect values for each CpG
+#' This function calculates multiple epigenetic score based on methylation data. It requires a beta matrix and a directory containing EWAS files.
 #'
 #' @import readxl
 #' @import dplyr
 #' @param thrs.criteria Thresholding criteria for CpG selection.
-#' @param beta Beta from sample CpG.
-#' @param ewas.file Path to the list of EWAS files.
+#' @param beta A preloaded beta matrix (data.frame) with CpG methylation values.
+#' @param ewas.file Path to the directory of EWAS files.
 #' @param missingness Maximum tolerated missing data for CpG sites.
-#' @return The calculated episcore.
+#' @param path Directory to save log files.
+#' @return A data.frame with calculated episcores for all EWAS files.
 #' @export
 #'
 calculate_multiple_episcores <- function(thrs.criteria = 0.05,
-                                            beta.file = "",
+                                            beta = "",
                                             ewas.list.path = "",
                                             missingness = 0.2,
                                             path = getwd()){
 
+  if (!file.exists(beta)) stop("Beta file does not exist.")
+  if (!dir.exists(ewas.dir)) stop("EWAS directory does not exist.")
+  
   sumstatslist <- list.files(ewas.list.path, full.names = T, pattern = "\\.txt$|\\.xlsx$")
-
-  file_ext <- tools::file_ext(beta.file)
-  beta <- if (file_ext == "xlsx") {
-    read_xlsx(beta.file, col_names = TRUE)
-  } else {
-    read.table(beta.file, header = TRUE)
-  }
-
-  epi <- data.frame(id = colnames(beta)[-1])  # Assuming 'beta' is already a data.frame
-
+  epi <- data.frame(id = colnames(beta)[-1])
   all_logs <- character()
+  
   for (file in sumstatslist) {
     tryCatch({
-      epi0 <- calculate_episcore(thrs.criteria, beta.file = beta.file, ewas.file = file, missingness = missingness, path = path)
+      epi0 <- calculate_episcore(thrs.criteria = thrs.criteria,
+                                 beta = beta,
+                                 ewas.file = file,
+                                 missingness = missingness,
+                                 path = path)
       epi <- cbind(epi, setNames(epi0, tools::file_path_sans_ext(basename(file))))
     }, error = function(e) {
       error_log <- paste("Failed to process", basename(file), ":", e$message, "\n")
