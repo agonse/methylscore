f491ba838d82113e990ea6d3421a1a51f3670ad7
Alex G Segura <83952268+agonse@users.noreply.github.com>
2024-12-19 10:11:07 +0100

    Update calculate_multiple_episcores.R

--- R/calculate_multiple_episcores.R
+++ R/calculate_multiple_episcores.R
@@ -15,22 +15,22 @@
 #' @export
 #'
 calculate_multiple_episcores <- function(thrs.criteria = 0.05,
-                                            beta = "",
+                                            beta.file = "",
                                             ewas.list.path = "",
                                             missingness = 0.2,
                                             path = getwd()){
 
-  if (!file.exists(beta)) stop("Beta file does not exist.")
+  if (!file.exists(beta.file)) stop("Beta file does not exist.")
   if (!dir.exists(ewas.dir)) stop("EWAS directory does not exist.")
   
   sumstatslist <- list.files(ewas.list.path, full.names = T, pattern = "\\.txt$|\\.xlsx$")
-  epi <- data.frame(id = colnames(beta)[-1])
+  epi <- data.frame(id = colnames(beta.file)[-1])
   all_logs <- character()
   
   for (file in sumstatslist) {
     tryCatch({
       epi0 <- calculate_episcore(thrs.criteria = thrs.criteria,
-                                 beta = beta,
+                                 beta.file = beta.file,
                                  ewas.file = file,
                                  missingness = missingness,
                                  path = path)
